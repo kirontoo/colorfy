ab086ab8ef0d36b6921771371950e01355a19508
Amy Dang <kirontoo@gmail.com>
2021-11-17 13:29:35 -0800

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./components/Home";
+import ColorBook from "./components/ColorBook";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/colorbook">Color Book</Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/colorbook" element={<ColorBook/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
