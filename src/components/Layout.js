cb207e14c53afe912dae7934c0db95dcffd50f08
Amber Terry <amber.l.terry@gmail.com>
2021-11-18 13:47:32 -0700

    Landing Page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,37 +1,25 @@
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function Layout({ 
-  navLinks,
-  brand, 
-  children 
-}) {
-  return (
-    <Router>
-      <Navbar 
-        bg="dark" 
-        expand="lg"
-        variant="dark"
-      >
-        <Container>
-          <Navbar.Brand href="/">{brand}</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-            <Nav className="my-2 my-2-lg">
-              {navLinks}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Container as="main" className="my-4">
-        {children}
-      </Container>
-    </Router>
-  )
-};
+function Layout({ navLinks, brand, children }) {
+    return (
+        <Router>
+            <Navbar bg="dark" expand="lg" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">{brand}</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                        <Nav className="my-2 my-2-lg">{navLinks}</Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Container as="main" className="my-4">
+                {children}
+            </Container>
+        </Router>
+    );
+}
 
 export default Layout;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,26 @@
+import React from 'react';
+import './Home.css';
+
 function Home() {
-  return (
-    <>
-      <h2>Home page</h2>
-    </>
-  );
+    return (
+        <>
+            <div className="app-header">
+                <div className="app-description"></div>
+                <div className="app-features"></div>
+                <div className="demo-video"></div>
+                <div className="app-tutorial"></div>
+                <div className="app-engineers">
+                    <div className="engineer-amy"></div>
+                    <div className="engineer-amber">
+                        <div className="amber-profile">
+                            <div className="amber-picture-background"></div>
+                        </div>
+                        <div className="amber-intro"></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Home;
