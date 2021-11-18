0885735b00c993c2bbdbc61bba4bc2751085a3ed
Amy Dang <kirontoo@gmail.com>
2021-11-17 16:19:19 -0800

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
   Link
 } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import {Form, FormControl, Button, Navbar, Nav} from 'react-bootstrap'
-import Home from "./components/Home";
-import ColorBook from "./components/ColorBook";
+import Layout from "./components/Layout";
+import Button from "react-bootstrap/Button";
+import Home from "./pages/Home";
+import ColorBook from "./pages/ColorBook";
+
+function NavLinks() {
+  return(
+    <>
+      <Link to="/colorbook">
+        <Button>
+          Try our app! 
+        </Button>
+      </Link>
+    </>
+  )
+}
 
 function App() {
+  const BRAND = "Colorfy"
   return (
-    <Router>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand>React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="">
-            <Nav className="my-2 my-2-lg">
-              <Nav.Link>
-                <Link to="/">Home</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to="/colorbook">Color Book</Link>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Container as="main">
-        <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route path="/colorbook" element={<ColorBook/>} />
-        </Routes>
-      </Container>
-    </Router>
+    <Layout 
+      brand={BRAND} 
+      navLinks={<NavLinks />}
+    >
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/colorbook" element={<ColorBook/>} />
+      </Routes>
+    </Layout>
   );
 }
 
