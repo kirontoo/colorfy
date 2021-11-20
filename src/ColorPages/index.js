f41a69b9e108cd86a2c75c7f324713378e2c9bd8
Amy Dang <kirontoo@gmail.com>
2021-11-19 18:10:45 -0800

    Update index

--- src/ColorPages/index.js
+++ src/ColorPages/index.js
@@ -1,10 +1,7 @@
-// import { ReactComponent as Lion } from './lion.svg';
-// import { ReactComponent as Lighthouse } from './lighthouse2.svg';
-// import { ReactComponent as Jackolantern } from './jackolantern.svg';
+import { ReactComponent as Taskade } from "./taskade.svg";
+import { ReactComponent as City } from "./city.svg";
 
-// TODO: find svg files that is imported for JSX
 export default {
-  Pages: [ 'lion', 'lighthouse', 'jackolantern'],
-  SvgFiles: {
-  }
+  "taskade": Taskade,
+  "city": City
 }
