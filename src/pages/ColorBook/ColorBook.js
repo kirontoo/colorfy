235c46407c8fc259fae987b7a30577e0ce36db6b
Amy Dang <kirontoo@gmail.com>
2021-11-19 16:23:41 -0800

    Add color picker and palettes

--- src/pages/ColorBook/ColorBook.js
+++ src/pages/ColorBook/ColorBook.js
@@ -2,6 +2,7 @@ import {
   Image,
   Stack,  
   Container,
+  Form,
   FormGroup,
   FormLabel,
   FormSelect,
@@ -10,10 +11,12 @@ import {
   Dropdown,
   Button,
 } from "react-bootstrap";
+import "./ColorBook.css";
 
 import { useState } from "react";
 import { renderToStaticMarkup } from 'react-dom/server';
 import ColorPages from "../../ColorPages";
+import { Palettes } from "./Palettes";
 
 function LoadSvg({svg}) {
   console.log("svg", svg);
@@ -23,59 +26,106 @@ function LoadSvg({svg}) {
 
 function ColorBook() {
   let [ coloringPage, setColoringPage ] = useState(ColorPages.Pages[0]);
+  let [ currentPalette, setCurrentPalette ] = useState("amethyst");
+  let [ currentColor, setCurrentColor ] = useState(`#${Palettes.amethyst[0]}`);
 
   function onSelectColoringPage(event) {
     setColoringPage(ColorPages.Pages[ event.target.value ]);
   }
 
+  function onSelectPalette(event) {
+    setCurrentPalette(event.target.value);
+  }
+
+  function onSelectColor(event) {
+    let color = event.target.value || event.target.style.background;
+    setCurrentColor(color);
+  }
+
   return (
     <Stack direction="vertical" gap={3} >
       <h3>Colorbook</h3>
-      <Stack 
-        direction="horizontal" 
-        gap={2}
+      <Row 
         className="align-items-start"
+        style={{columnGap: "1rem", rowGap: "1rem"}}
       >
         <Col 
+          sm={12}
           lg={3} 
-          className="bg-light p-4 rounded-3 d-flex flex-column"
+          className="bg-light p-4 shadow-sm rounded-3 d-flex flex-column"
           style={{ rowGap: "1rem" }}
         >
-          <FormGroup controlId="formColorPage">
+          <FormGroup>
             <FormLabel>Select coloring page</FormLabel>
             <FormSelect 
               ariaLabel="Select coloring page" 
               onChange={onSelectColoringPage}
+              value={coloringPage}
             >
               { ColorPages.Pages.map((name, index) => {
-                  return (
-                    <option key={`${name}-${index}`} value={index}>{name}</option>
-                  )
-                })
+                return (
+                  <option key={`${name}-${index}`} value={index}>{name}</option>
+                )
+              })
               }
             </FormSelect>
           </FormGroup>
 
-          <FormGroup controlId="formColorPalette">
+          <FormGroup>
+            <Form.Label htmlFor="exampleColorInput">Color picker</Form.Label>
+            <Form.Control
+              type="color"
+              id="exampleColorInput"
+              defaultValue="#563d7c"
+              title="Choose your color"
+              onChange={onSelectColor}
+              />
+          </FormGroup>
+
+          <Container>
+            <Row>
+              <Col></Col>
+            </Row>
+          </Container>
+
+          <FormGroup>
             <FormLabel>Select Color Palette</FormLabel>
             <FormSelect 
               ariaLabel="Select coloring page" 
+              onChange={onSelectPalette}
+              value={currentPalette}
             >
-              { ColorPages.Pages.map((name, index) => {
-                  return (
-                    <option key={`${name}-${index}`} value={index}>{name}</option>
-                  )
-                })
+              { Object.keys(Palettes).map((name, index) => {
+                return (
+                  <option key={`${name}-${index}`} value={name}>{name}</option>
+                )
+              })
               }
             </FormSelect>
           </FormGroup>
 
+          <Container>
+            <ul className="d-flex flex-wrap m-0 p-0 palette">
+              { 
+                Palettes[currentPalette].map(( color ) => {
+                  return (
+                    <li 
+                      className="p-3 m-1 rounded-2 shadow-sm palette__swatch" 
+                      style={{background: `#${color}`}}
+                      onClick={onSelectColor}
+                    />
+                  )
+                })
+              }
+            </ul>
+          </Container>
           {/** TODO: color palets here? **/}
         </Col>
-        <Col>
-          {coloringPage}
+        <Col className="bg-light p-4 shadow-sm rounded-3 min-vh-75">
+          {`coloring page: ${coloringPage}\n`}
+          {`selected color: ${currentColor}`}
         </Col>
-      </Stack>
+      </Row>
     </Stack>
   );
 };
