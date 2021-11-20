41718ae6ed3725a02ca3f068ffe560cca1a1b7a2
Amy Dang <kirontoo@gmail.com>
2021-11-20 13:13:02 -0800

    Add coloring pages

--- src/ColorPages/index.js
+++ src/ColorPages/index.js
@@ -1,7 +1,15 @@
 import { ReactComponent as Taskade } from "./taskade.svg";
 import { ReactComponent as City } from "./city.svg";
+import { ReactComponent as Postgraphile } from "./postgraphile.svg";
+import { ReactComponent as Dojo } from "./dojo.svg";
+import { ReactComponent as Gopher } from "./gopher.svg";
+import { ReactComponent as HostGator } from "./hostgator.svg";
 
 export default {
   "taskade": Taskade,
-  "city": City
+  "city": City,
+  "postgraphile": Postgraphile,
+  "dojo": Dojo,
+  "gopher": Gopher,
+  "hostgator": HostGator,
 }
