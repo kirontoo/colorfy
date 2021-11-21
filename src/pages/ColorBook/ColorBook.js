e49983acd4fc8eac0375bd746f398e8299e2a5ba
Amy Dang <kirontoo@gmail.com>
2021-11-21 12:13:30 -0800

    Remove padding

--- src/pages/ColorBook/ColorBook.js
+++ src/pages/ColorBook/ColorBook.js
@@ -52,8 +52,8 @@ function ColorBook() {
   }
 
   return (
-    <Stack direction="vertical" gap={3} >
-      <h3 className="w-100 text-align-center">Colorbook</h3>
+    <Stack direction="vertical" gap={3} style={{paddingBottom: "2rem"}}>
+      <h3 className="w-100 text-align-center p-0">Colorbook</h3>
       <Row 
         className="align-items-start"
         style={{columnGap: "1rem", rowGap: "1rem"}}
